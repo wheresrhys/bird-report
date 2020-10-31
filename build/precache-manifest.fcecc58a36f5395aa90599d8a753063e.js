bb89ecd6574c98578475c0f3d12b002e0b0769b5
Rhys Evans <wheresrhys@gmail.com>
2020-10-31 17:52:41 +0000

    commit build

---
+++ build/precache-manifest.fcecc58a36f5395aa90599d8a753063e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6c61f3cabbb23e9058a98855c4d346",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e744c737f7dc8f2ce10",
+    "url": "./static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "eda10c598577eebd2aa1",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4e744c737f7dc8f2ce10",
+    "url": "./static/js/2.ad159841.chunk.js"
+  },
+  {
+    "revision": "13e3e99786fbe129df92af633db0402c",
+    "url": "./static/js/2.ad159841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda10c598577eebd2aa1",
+    "url": "./static/js/main.d8029b6c.chunk.js"
+  },
+  {
+    "revision": "3e9ee90b5ea58a3cc473",
+    "url": "./static/js/runtime-main.926bf00d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
