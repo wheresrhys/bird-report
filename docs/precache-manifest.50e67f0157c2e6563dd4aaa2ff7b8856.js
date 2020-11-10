eb20289f7a26a12315784fd2770ffe4c44b8aae8
Rhys Evans <wheresrhys@gmail.com>
2020-11-10 21:31:02 +0000

    build

---
+++ docs/precache-manifest.50e67f0157c2e6563dd4aaa2ff7b8856.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3a9bc28def28a39be1d5193e9f9a83",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5c8743d41f1f0fef8b3",
+    "url": "./static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "01bf535c9a84131824ab",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e5c8743d41f1f0fef8b3",
+    "url": "./static/js/2.00d17dee.chunk.js"
+  },
+  {
+    "revision": "1e21e80a51252385c73cfddbbe91ab8b",
+    "url": "./static/js/2.00d17dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01bf535c9a84131824ab",
+    "url": "./static/js/main.60f7c4bc.chunk.js"
+  },
+  {
+    "revision": "3e9ee90b5ea58a3cc473",
+    "url": "./static/js/runtime-main.926bf00d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.dd3be469662514d76751362ed6365c23.js"
+  "./precache-manifest.50e67f0157c2e6563dd4aaa2ff7b8856.js"
 );
 
 self.addEventListener('message', (event) => {
