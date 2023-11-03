410f404c86355ae774daf64bbc118b7fdeb35d0a
Rhys Evans <rhys.evans@ft.com>
2023-11-03 18:50:23 +0000

    rebuild

--- docs/internal/immutable/chunks/3-12cd9533.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-87a56e23.js";import{default as t}from"../components/pages/_bird_/_page.svelte-bcc4e894.js";export{t as component,r as universal};
---
+++ docs/internal/immutable/chunks/3-f6a9c1bd.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-87a56e23.js";import{default as t}from"../components/pages/_bird_/_page.svelte-206a0898.js";export{t as component,r as universal};
